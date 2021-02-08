0119eeec9d3efc027e0332953170a4d2902d65b6
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-08 10:04:45 +0100

    Create upload_clinvar.py

---
+++ upload_clinvar.py
@@ -0,0 +1,25 @@
+import os
+
+import biothings, config
+biothings.config_for_app(config)
+
+import biothings.hub.dataload.uploader
+
+from .parser import load_occurrences
+
+
+class OccurrencesUploader(biothings.hub.dataload.uploader.BaseSourceUploader):
+
+    main_source = "test5"
+    name = "clinvar"
+    __metadata__ = {"src_meta": {}}
+    idconverter = None
+    storage_class = biothings.hub.dataload.storage.BasicStorage
+
+    def load_data(self, data_folder):
+        self.logger.info("Load data from directory: '%s'" % data_folder)
+        return load_clinvar(data_folder)
+
+    @classmethod
+    def get_mapping(klass):
+        return {}
